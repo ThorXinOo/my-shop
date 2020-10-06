4ea766c5a0c8b998a6f35df43baa7690b7d6ec04
Thor Xin Oo <thawzin36978@gmail.com>
2020-10-06 10:44:05 +0630

    Navbar Components 2

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,67 @@
 /** @format */
 
 import React from 'react';
+import { FaBars, FaCartPlus } from 'react-icons/fa';
+import styled from 'styled-components';
+import { ProductConsumer } from '../context';
+import logo from '../images/logo.svg';
 
 export default function Navbar() {
-  return <div>Hello I am Navbar</div>;
+  return (
+    <ProductConsumer>
+      {(value) => {
+        const { cartItems, handleSidebar, handleCart } = value;
+        return (
+          <NavWrapper>
+            <div className='nav-center'>
+              <FaBars className='nav-icon' onClick={handleSidebar} />
+              <img src={logo} alt='tech store logo' />
+              <div className='nav-cart'>
+                <FaCartPlus className='nav-icon' onClick={handleCart} />
+                <div className='cart-items'>{cartItems}</div>
+              </div>
+            </div>
+          </NavWrapper>
+        );
+      }}
+    </ProductConsumer>
+  );
 }
+
+const NavWrapper = styled.nav`
+  position: sticky;
+  position: --webkit-sticky;
+  top: 0;
+  width: 100%;
+  padding: 1rem 1.5rem;
+  background: var(--mainGrey);
+  border-bottom: 3px solid var(--primaryColor);
+
+  .nav-center {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    max-width: 1170px;
+    margin: 0 auto;
+    /* border: 1px solid #000; */
+  }
+
+  .nav-icon {
+    font-size: 1.5rem;
+    cursor: pointer;
+  }
+
+  .nav-cart {
+    position: relative;
+  }
+  .cart-items {
+    background: var(--primaryColor);
+    color: var(--mainWhite);
+    font-size: 0.85rem;
+    position: absolute;
+    right: -8px;
+    top: -8px;
+    padding: 0 5px;
+    border-radius: 50%;
+  }
+`;
