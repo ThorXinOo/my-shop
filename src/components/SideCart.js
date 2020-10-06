f420e21598b0758dfa1693dd8b0ce093b08e6d55
Thor Xin Oo <thawzin36978@gmail.com>
2020-10-06 18:51:27 +0630

    SideCart Component

--- src/components/SideCart.js
+++ src/components/SideCart.js
@@ -1,7 +1,37 @@
 /** @format */
 
 import React from 'react';
+import styled from 'styled-components';
+import { ProductConsumer } from '../context';
 
 export default function SideCart() {
-  return <div>I am Side Cart</div>;
+  return (
+    <ProductConsumer>
+      {(value) => {
+        const { cartOpen, closeCart, cart } = value;
+        // console.log('some');
+        return (
+          <SideCartWrapper show={cartOpen} onClick={closeCart}>
+            <p>Cart Items</p>
+          </SideCartWrapper>
+        );
+      }}
+    </ProductConsumer>
+  );
 }
+
+const SideCartWrapper = styled.div`
+  position: fixed;
+  top: 61px;
+  right: 0;
+  width: 100%;
+  height: 100%;
+  background: var(--mainGrey);
+  z-index: 1;
+  border-left: 4px solid var(--primaryColor);
+  transition: var(--mainTransition);
+  transform: ${(props) => (props.show ? 'translateX(0)' : 'translateX(100%)')};
+  @media (min-width: 576px) {
+    width: 20rem;
+  }
+`;
