1ce0a0eb65f580979504ebe14fc6497958757c44
Thor Xin Oo <thawzin36978@gmail.com>
2020-10-09 20:41:31 +0630

    All Product page and section

--- src/pages/ProductsPage.js
+++ src/pages/ProductsPage.js
@@ -1,11 +1,15 @@
 /** @format */
 
 import React from 'react';
+import Products from '../components/ProductPage/Products';
+import Hero from '../components/Hero';
+import productBcg from '../images/productsBcg.jpeg';
 
 export default function ProductsPage() {
   return (
     <>
-      <h1>Hello From Products Page</h1>
+      <Hero img={productBcg} />
+      <Products />
     </>
   );
 }
