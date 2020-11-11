6fe3dedb26f9196e1cd77bdcfd541b10f094f9f0
Thor Xin Oo <thawzin36978@gmail.com>
2020-11-11 22:44:25 +0630

    Sidecart component

--- src/components/SideCart.js
+++ src/components/SideCart.js
@@ -3,16 +3,43 @@
 import React from 'react';
 import styled from 'styled-components';
 import { ProductConsumer } from '../context';
+import { Link } from 'react-router-dom';
 
 export default function SideCart() {
   return (
     <ProductConsumer>
       {(value) => {
-        const { cartOpen, closeCart, cart } = value;
+        const { cartOpen, closeCart, cart, cartTotal } = value;
         // console.log('some');
         return (
           <SideCartWrapper show={cartOpen} onClick={closeCart}>
             <p>Cart Items</p>
+            <ul>
+              {cart.map((item) => {
+                return (
+                  <li key={item.id} className='cart-item mb-4'>
+                    <img src={`../${item.image} `} alt='' width='35' />
+                    <div className='mt-3'>
+                      <h6 className='text-uppercase'>{item.title}</h6>
+                      <h6 className='text-title text-capitalize'>
+                        count : {item.count}
+                      </h6>
+                      <h6 className='text-title text-capitalize'>
+                        amount : {item.price}
+                      </h6>
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
+            <h4 className='text-capitalize text-main'>
+              cart total : {cartTotal}
+            </h4>
+            <div className='text-center my-5'>
+              <Link to='/cart' className='main-link'>
+                cart page
+              </Link>
+            </div>
           </SideCartWrapper>
         );
       }}
@@ -33,5 +60,13 @@ const SideCartWrapper = styled.div`
   transform: ${(props) => (props.show ? 'translateX(0)' : 'translateX(100%)')};
   @media (min-width: 576px) {
     width: 20rem;
+  }
+  overflow: scroll;
+  padding: 2rem;
+  ul {
+    padding: 0 !important;
+  }
+  .cart-item {
+    list-style-type: none;
   }
 `;
