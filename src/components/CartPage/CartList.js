3e5f3f5297a4baa09407aaa588d223d2f28425c6
Thor Xin Oo <thawzin36978@gmail.com>
2020-11-13 07:42:25 +0630

    CartList component in CartPage without CartItem component

--- src/components/CartPage/CartList.js
+++ src/components/CartPage/CartList.js
@@ -2,11 +2,40 @@
 
 import React from 'react';
 import CartItem from './CartItem';
+import { ProductConsumer } from '../../context';
 
 export default function CartList() {
   return (
-    <div>
-      <CartItem></CartItem>
+    <div className='container-fluid'>
+      <div className='row'>
+        <div className='col'>
+          <ProductConsumer>
+            {(value) => {
+              const { cart, removeitem, increment, decrement } = value;
+              if (cart.length === 0) {
+                return (
+                  <h1 className='text-title text-center my-4'>
+                    Your Cart is currently Empty
+                  </h1>
+                );
+              }
+              return (
+                <>
+                  {cart.map((item) => (
+                    <CartItem
+                      key={item.id}
+                      cartItem={item}
+                      increment={increment}
+                      decrement={decrement}
+                      removeitem={removeitem}
+                    ></CartItem>
+                  ))}
+                </>
+              );
+            }}
+          </ProductConsumer>
+        </div>
+      </div>
     </div>
   );
 }
