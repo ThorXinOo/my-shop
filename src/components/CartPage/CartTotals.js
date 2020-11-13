ab18dd3044251456679a9b7d12995090d8bbac55
Thor Xin Oo <thawzin36978@gmail.com>
2020-11-13 07:29:39 +0630

    CartTotals in Cart Page

--- src/components/CartPage/CartTotals.js
+++ src/components/CartPage/CartTotals.js
@@ -1,7 +1,28 @@
 /** @format */
 
 import React from 'react';
+import { ProductConsumer } from '../../context';
 
 export default function CartTotals() {
-  return <div>Carts Totals</div>;
+  return (
+    <div className='container'>
+      <div className='row'>
+        <ProductConsumer>
+          {(value) => {
+            const { cartSubTotal, cartTax, cartTotal, clearCart } = value;
+            return (
+              <div className='col text-title text-center my-4'>
+                <button className='btn btn-outline-danger text-capitalize mb-4'>
+                  Clear Cart
+                </button>
+                <h3>SubTotal : ${cartSubTotal}</h3>
+                <h3>Tax : ${cartTax}</h3>
+                <h3>Total : ${cartTotal}</h3>
+              </div>
+            );
+          }}
+        </ProductConsumer>
+      </div>
+    </div>
+  );
 }
